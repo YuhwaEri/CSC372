c92715d3704999996f9a4796a29928d09b90d37e
tou <tou@email.arizona.edu>
2022-04-10 18:17:48 -0700

    Terminals added

--- Project2/src/parser/Main.java
+++ Project2/src/parser/Main.java
@@ -8,18 +8,22 @@ import java.util.Scanner;
 public class Main {
 	//private static Pattern var_assign = Pattern.compile("^(.+) = (.+)\\.$");
 	private static Pattern bool_op = Pattern.compile("AND|OR|XOR|NOT|XAND");
-
-	
+	private static Pattern bool = Pattern.compile("true|false");
+	private static Pattern digits = Pattern.compile("\\d+");
+	private static Pattern comp = Pattern.compile("<|>|==|<=|>=");
+	private static Pattern chr = Pattern.compile("([A-Z]|[a-z]){1}");
+	/*private static Pattern bool_expr = Pattern.compile("(" + bool.pattern() + "){1}"+ "|" 
+	+ "(" + bool.pattern() + "){1} (" + bool_op.pattern() + "){1}");*/
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		System.out.print(">> ");
 		String cmd = in.nextLine();
 		while(!cmd.equals("exit")) {
-			System.out.print(bool_op.matcher("AND").matches());
+			System.out.print(chr.matcher(cmd).matches());
+			System.out.println();
+			System.out.print(">> ");
 		cmd = in.nextLine();
 		}
-		
 		in.close();
-
 	}
 }
