aa2b0b67adbf12b26e98a39525ef040a89cd1319
tou <tou@email.arizona.edu>
2022-04-10 19:25:48 -0700

    Var object added

--- Project2/src/parser/Main.java
+++ Project2/src/parser/Main.java
@@ -2,16 +2,20 @@ package parser;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class Main {
 	//private static Pattern var_assign = Pattern.compile("^(.+) = (.+)\\.$");
+	private static ArrayList<Var> var_list = new ArrayList<Var>();
 	private static Pattern bool_op = Pattern.compile("AND|OR|XOR|NOT|XAND");
 	private static Pattern bool = Pattern.compile("true|false");
 	private static Pattern digits = Pattern.compile("\\d+");
 	private static Pattern comp = Pattern.compile("<|>|==|<=|>=");
 	private static Pattern chr = Pattern.compile("([A-Z]|[a-z]){1}");
+	private static Pattern var = Pattern.compile("\\w+");
+	private static Pattern var_assg = Pattern.compile("(.+) = (.+)");
 	/*private static Pattern bool_expr = Pattern.compile("(" + bool.pattern() + "){1}"+ "|" 
 	+ "(" + bool.pattern() + "){1} (" + bool_op.pattern() + "){1}");*/
 	public static void main(String[] args) {
@@ -25,5 +29,19 @@ public class Main {
 		cmd = in.nextLine();
 		}
 		in.close();
+	}
+		
+	static Var get_var_obj(String name) {
+		for(int i=0; i<var_list.size(); i++) {
+			if(var_list.get(i).name == name){
+				return var_list.get(i);
+			}
+		}
+		return null;
+	}
+	
+	boolean evaluateBExpr(String cmd) {
+		// x = true
+		return true;
 	}
 }
