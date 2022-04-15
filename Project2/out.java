2e73df6cf164736a5095cb9f283f1f9f38ad0ee5
tou <tou@email.arizona.edu>
2022-04-15 02:55:39 -0700

    Formatting cleaned up, nests should display correctly

--- Project2/out.java
+++ Project2/out.java
@@ -2,24 +2,8 @@ public class out{
 	public static void main(String args[]) {
 		int x = 7;
 		if(x>5){
-	int x = 5;
-	int x = 6;
-}
+			int x = 5;
+		}
+		int x = 6;
 	}
 }
-
-
-/*Expected
-public class out{
-	public static void main(String args[]) {
-		int x = 7;
-		if(x>5){
-	 	x = 5;
-}
-		x = 6;
-	}
-}
-
-
-
- */
